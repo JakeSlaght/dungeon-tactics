49d57d5909bd5fe204c1e71f75d9f1730a19671a
JakeSlaght <jake.r.slaght@gmail.com>
2025-05-16 20:58:49 -0500

    feat: shifting to a left side tab setup for creation, right will display character sheet as it is created

--- dungeon-tactics/character_sheet_components/creation/character_creation.gd
+++ dungeon-tactics/character_sheet_components/creation/character_creation.gd
@@ -10,22 +10,15 @@ class_name CharacterCreation extends Node
 @onready var saving_throws: Label = %saving_throws
 @onready var profiencies: Label = %profiencies
 
-enum ROLL_OPTION {
-	STANDARD,
-	LOWEST_OUT,
-	POINT_BUY
-}
-
-var option_chosen: ROLL_OPTION = ROLL_OPTION.LOWEST_OUT
 var class_type_chosen: CONSTANTS.Class_Type
 
 var rolls = {
-	'roll_1': 0,
-	'roll_2': 0,
-	'roll_3': 0,
-	'roll_4': 0,
-	'roll_5': 0,
-	'roll_6': 0
+	'roll_1': 8,
+	'roll_2': 8,
+	'roll_3': 8,
+	'roll_4': 8,
+	'roll_5': 8,
+	'roll_6': 8
 }
 var toggles = {
 	'minus': false,
@@ -38,6 +31,7 @@ func _ready() -> void:
 	Loader.load()
 	Events.plus_minus_roll_trigger.connect(adjust_attribute)
 	class_options.item_selected.connect(on_class_selection)
+	_roll_selection()
 
 func _class_filteration() -> void:
 	class_options.clear()
@@ -48,77 +42,13 @@ func _class_filteration() -> void:
 		class_options.add_item(c.name)
 
 func _roll_selection() -> void:
-	point_buy_box.visible = false
-	toggles['minus'] = false
-	toggles['plus'] = false
-	match option_chosen:
-		ROLL_OPTION.LOWEST_OUT:
-			get_tree().call_group('lr_attributes', 'process_plus_minus_toggle', toggles)
-			_roll_dice()
-		ROLL_OPTION.POINT_BUY:
-			rolls = {
-			'roll_1': 8,
-			'roll_2': 8,
-			'roll_3': 8,
-			'roll_4': 8,
-			'roll_5': 8,
-			'roll_6': 8
-			}
-			toggles['minus'] = true
-			toggles['plus'] = true
-			point_buy_box.visible = true
-			remaining_points.text = '%s' %[current_points]
-			total_points.text = '/%s' %[total_max_points]
-			get_tree().call_group('lr_attributes', 'process_plus_minus_toggle', toggles)
-		ROLL_OPTION.STANDARD:
-			get_tree().call_group('lr_attributes', 'process_plus_minus_toggle', toggles)
-			rolls = {
-			'roll_1': 15,
-			'roll_2': 14,
-			'roll_3': 13,
-			'roll_4': 12,
-			'roll_5': 10,
-			'roll_6': 8
-			}
-
-	get_tree().call_group('lr_attributes', 'process_lr_attribute_data', rolls)
-
-func _roll_dice() -> void:
-	rolls = {
-		'roll_1': 0,
-		'roll_2': 0,
-		'roll_3': 0,
-		'roll_4': 0,
-		'roll_5': 0,
-		'roll_6': 0
-	}
-
-	for i in range(0, 6):
-		var die1: int = randi_range(1,6)
-		var die2: int = randi_range(1,6)
-		var die3: int = randi_range(1,6)
-		var die4: int = randi_range(1,6)
-		var dice: Array[int] = [die1, die2, die3, die4]
-
-		var roll_string = 'roll_%s' %[i+1]
-		rolls[roll_string] = _remove_lowest(dice)
-   
-	get_tree().call_group('lr_attributes', 'process_lr_attribute_data', rolls)
-
-func _remove_lowest(dice) -> int:
-	var lowest = dice[0]
-	for i in range(1, len(dice)):
-		if dice[i] < lowest:
-			lowest = dice[i]
-   
-	var index_to_remove: int = dice.find(lowest)
-	var sum: int = 0
-   
-	for i in range(len(dice)):
-		if i != index_to_remove:
-			sum += dice[i]
-   
-	return sum
+	toggles['minus'] = true
+	toggles['plus'] = true
+	point_buy_box.visible = true
+	remaining_points.text = '%s' %[current_points]
+	total_points.text = '/%s' %[total_max_points]
+	get_tree().call_group('create_stat_blocks', 'process_plus_minus_toggle', toggles)
+	get_tree().call_group('create_stat_blocks', 'process_lr_attribute_data', rolls)
 
 func adjust_attribute(plus_minus: String, roll_indicator: String) -> void:
 	var changed_value = rolls[roll_indicator]
@@ -141,8 +71,8 @@ func adjust_attribute(plus_minus: String, roll_indicator: String) -> void:
 		toggles['minus'] = true
 		toggles['plus'] = true
 
-	get_tree().call_group('lr_attributes', 'process_plus_minus_toggle', toggles)
-	get_tree().call_group('lr_attributes', 'process_lr_attribute_data', rolls)
+	get_tree().call_group('create_stat_blocks', 'process_plus_minus_toggle', toggles)
+	get_tree().call_group('create_stat_blocks', 'process_lr_attribute_data', rolls)
 	remaining_points.text = '%s' %[current_points]
 
 func on_class_selection(selection: int) -> void:
@@ -166,18 +96,6 @@ func on_class_selection(selection: int) -> void:
 
 	mc_skill_requirements.text = mc_reqs
 	saving_throws.text = saving_throw_string
-
-func _on_btn_advanced_pressed() -> void:
-	option_chosen = ROLL_OPTION.POINT_BUY
-	_roll_selection()
-
-func _on_btn_standard_pressed() -> void:
-	option_chosen = ROLL_OPTION.LOWEST_OUT
-	_roll_selection()
-
-func _on_btn_easy_pressed() -> void:
-	option_chosen = ROLL_OPTION.STANDARD
-	_roll_selection()
 
 func _on_btn_melee_pressed() -> void:
 	class_type_chosen = CONSTANTS.Class_Type.MELEE
--- dungeon-tactics/character_sheet_components/lr_attribute.gd
+++ dungeon-tactics/character_sheet_components/lr_attribute.gd
@@ -29,25 +29,12 @@ func process_plus_minus_toggle(toggle_bool) -> void:
 	plus_button.visible = toggle_bool['plus']
 	toggle_plus_minus = toggle_bool
 
-# for both minus and plus we need to check if the value 
-# is between 8 and 15. If it is 8 we do not do the minus, 
-# if it is 15 we do not do the plus
-
 func _on_minus_button_pressed() -> void:
-	print('test on click minus', attribute_modifer.get_text())
 	var attribute_modifer_value = int(attribute_modifer.get_text())
-	print_debug(attribute_modifer_value)
 	if attribute_modifer_value > 8:
 		Events.plus_minus_roll_trigger.emit('minus', attribute_key)
-		print_debug('we are able to go ABOVE 8')
-	else:
-		print_debug('we are at 8....')
-
 
 func _on_plus_button_pressed() -> void:
 	var attribute_modifer_value = int(attribute_modifer.get_text())
 	if attribute_modifer_value < 15:
 		Events.plus_minus_roll_trigger.emit('plus', attribute_key)
-		print_debug('we are able to go BELOW 15')
-	else:
-		print_debug('we are at 15....')
